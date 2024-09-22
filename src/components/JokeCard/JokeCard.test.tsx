4c28b1e8e03501521a02f02ce16071bf8507c69f
DewitteRuben <rubendewitte1998@gmail.com>
2024-09-22 11:39:13 +0200

    chore: rename 'quote' to 'joke'

--- src/components/JokeCard/JokeCard.test.tsx
+++ src/components/JokeCard/JokeCard.test.tsx
@@ -4,7 +4,7 @@ import JokeCard from "./JokeCard";
 describe("JokeCard Component", () => {
   const joke = "Chuck Norris can divide by zero";
 
-  it("renders the joke quote", () => {
+  it("renders the joke", () => {
     render(<JokeCard joke={joke} />);
     const jokeElement = screen.getByText(joke);
     expect(jokeElement).toBeInTheDocument();
