fdb983a398655103ec535baa4c830a3fc1125b7c
DewitteRuben <rubendewitte1998@gmail.com>
2024-09-22 11:23:00 +0200

    chore: rename quote to joke

--- src/api/chuck-api.test.ts
+++ src/api/chuck-api.test.ts
@@ -2,7 +2,7 @@ import chuckAPI from "./chuck-api";
 
 describe("ChuckAPI", () => {
   it("should fetch a random quote", async () => {
-    const quote = await chuckAPI.getRandomQuote();
+    const quote = await chuckAPI.getRandomJoke();
 
     expect(quote).toHaveProperty("categories");
     expect(quote).toHaveProperty("created_at");
@@ -14,7 +14,7 @@ describe("ChuckAPI", () => {
   });
 
   it("should fetch multiple random quotes", async () => {
-    const quotes = await chuckAPI.getRandomQuotes(2);
+    const quotes = await chuckAPI.getRandomJokes(2);
 
     expect(quotes.length).toBe(2);
     quotes.forEach((quote) => {
