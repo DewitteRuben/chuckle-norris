94f3daa7bc52f03425de04f010e622720ed77718
DewitteRuben <rubendewitte1998@gmail.com>
2024-09-22 10:26:56 +0200

    feat: setup pre-commit linting using lintstaged, prettier and eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,36 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+
+
+export default [
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    settings: {
+      react: {
+        version: "detect"
+      }
+    },
+    files: ['**/*.{js,jsx,mjs,cjs,ts,tsx}'],
+    plugins: {
+      pluginReact,
+    },
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        ...globals.browser,
+      },
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
+  { ignores: ["build/*", "*.config.js"] },
+];
